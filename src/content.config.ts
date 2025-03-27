4f9b6e87c0ebea338bec61ef11329f0ca022b049
Kreedzt <zhaozisong1@live.com>
2025-03-27 09:05:39 +0800

    :bug: fixed notes time display

--- src/content.config.ts
+++ src/content.config.ts
@@ -44,8 +44,12 @@ const note = defineCollection({
 		description: z.string().optional(),
 		publishDate: z
 			.string()
-			.datetime({ offset: true }) // Ensures ISO 8601 format with offsets allowed (e.g. "2024-01-01T00:00:00Z" and "2024-01-01T00:00:00+02:00")
+			.or(z.date())
 			.transform((val) => new Date(val)),
+		updatedDate: z
+			.string()
+			.optional()
+			.transform((str) => (str ? new Date(str) : undefined)),
 	}),
 });
 
